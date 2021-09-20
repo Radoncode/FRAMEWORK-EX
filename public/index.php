799959025f42e3cf7cec8bcb56a9d00ace7a4252
Radoncode <relbouni69@gmail.com>
2021-09-20 19:03:19 +0100

    cutting up the application on some modules , the first one BlogModule

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use GuzzleHttp\Psr7\ServerRequest;
 
 require '../vendor/autoload.php';
 
-$app = new App();
+$app = new App([
+    BlogModule::class
+]);
 $response = $app->run(ServerRequest::fromGlobals());
 \Http\Response\send($response);
