de895d684b0ed16da001189a3699644aebf13df2
Radoncode <relbouni69@gmail.com>
2021-09-28 20:07:41 +0100

    adding possibilities of response

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -8,7 +8,20 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class App
 {   
+        
+    /**
+     * List of modules
+     *
+     * @var array
+     */
     private $modules = [];
+    
+    /**
+     * router
+     *
+     * @var Router
+     */
+    private $router;
 
     /**
      * __construct
@@ -16,9 +29,9 @@ class App
      */
     public function __construct(array $modules = [])
     {   
-        $router = new Router();
+        $this->router = new Router();
         foreach($modules as $module){
-            $this->modules[] = new $module($router);
+            $this->modules[] = new $module($this->router);
         }
     }
 
@@ -30,9 +43,17 @@ class App
                 ->withStatus(301)
                 ->withHeader('Location', substr($uri, 0, -1));
         }
-        if ($uri === '/blog') {
-            return new Response(200, [], '<h1>Welcome to my blog</h1>');
+        $route = $this->router->match($request);
+        if (is_null($route)){
+            return new Response(404, [], '<h1>Error 404</h1>');
         }
-        return new Response(404, [], '<h1>Error 404</h1>');
+        $response = call_user_func_array($route->getCallback(), [$request]);
+        if (is_string($response)) {
+            return new Response(200, [], $response);
+        } elseif ($response instanceof ResponseInterface) {
+            return $response;
+        } else {
+            throw new \Exception('The response is not a string or an instance of ResponseInterface');
+        }
     }
 }
