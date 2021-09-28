f9ba6db4169dd5012b35a111e2cb146fa47bf50c
Radoncode <relbouni69@gmail.com>
2021-09-28 20:06:36 +0100

    deleting namespace inused

--- src/Blog/BlogModule.php
+++ src/Blog/BlogModule.php
@@ -3,7 +3,6 @@
 namespace App\Blog;
 
 use Framework\Router;
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class BlogModule {
@@ -11,7 +10,7 @@ class BlogModule {
     public function __construct(Router $router)
     {
          $router->get('/blog', [$this, 'index'], 'blog.index');
-         $router->get('/blog/{slug}', [$this, 'show'], 'blog.show');
+         $router->get('/blog/{slug:[a-z\-]+}', [$this, 'show'], 'blog.show');
     }
 
     public function index(Request $request): string
@@ -21,6 +20,6 @@ class BlogModule {
 
     public function show(Request $request): string
     {   
-        return '<h1>Welcome to my post'.$request->getAttribute('id').'</h1>';
+        return '<h1>Welcome to my post'.$request->getAttribute('slug').'</h1>';
     } 
 }
