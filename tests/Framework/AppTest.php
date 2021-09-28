b3223e54f0b53d6c63fa25610e7372acacdc7fb7
Radoncode <relbouni69@gmail.com>
2021-09-28 20:09:04 +0100

    adding test for slug , exception,string response

--- tests/Framework/AppTest.php
+++ tests/Framework/AppTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Framework;
 
 use Framework\App;
+use App\Blog\BlogModule;
 use PHPUnit\Framework\TestCase;
 use GuzzleHttp\Psr7\ServerRequest;
-
+use Psr\Http\Message\ResponseInterface;
+use Tests\Framework\Modules\StringModule;
+use Tests\Framework\Modules\ErroredModule;
 
 class AppTest extends TestCase {
 
@@ -18,11 +21,37 @@ class AppTest extends TestCase {
     }
 
     public function testBlog() {
-        $app = new App();
+        $app = new App([
+            BlogModule::class
+        ]);
         $request = new ServerRequest('GET', '/blog');
         $response = $app->run($request);
         $this->assertContains('<h1>Welcome to my blog</h1>', (string)$response->getBody());
         $this->assertEquals(200, $response->getStatusCode());
+
+        $requestSingle = new ServerRequest('GET', '/blog/post-of-test');
+        $responseSingle = $app->run($requestSingle);
+        $this->assertContains('<h1>Welcome to my post post-of-test</h1>',(string)$responseSingle->getBody());
+    }
+
+    public function testThrowExceptionIfNoResponseSent () {
+        $app = new App([
+            ErroredModule::class
+        ]);
+        $request = new ServerRequest('GET', '/demo');
+        $this->expectException(\exception::class);
+        $app->run($request);
+
+    }
+
+    public function testConvertStringToResponse() {
+        $app = new App([
+            StringModule::class
+        ]);
+        $request = new ServerRequest('GET', '/demo');
+        $response = $app->run($request);
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals('DEMO', (string)$response->getBody());
     }
 
     public function testError404() {
